42e7e69a25ab11a2c823df75a7cc73ef8731d2c1
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-02-04 04:36:54 +0100

    Auto-generate hardware-configuration.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,21 @@
 {
     outputs = { self, nixpkgs }: {
-        nixosConfigurations.mae = nixpkgs.lib.nixosSystem {
+        nixosConfigurations.mae = nixpkgs.lib.nixosSystem rec {
             system = "x86_64-linux";
-            modules = [ ./hardware-configuration.nix ]
+            modules = let
+                pkgs = nixpkgs.legacyPackages.${system};
+
+                hardware-configuration =
+                    pkgs.runCommandLocal "hardware-configuration" {} ''
+                        ${pkgs.nixos-install-tools}/bin/nixos-generate-config \
+                            --no-filesystems --show-hardware-config > $out
+                    '';
+
+                impure-hardware-configuration = derivation ({
+                    __impureHostDeps = [ "/sys" ];
+                } // hardware-configuration.drvAttrs);
+
+            in [ impure-hardware-configuration.outPath ]
                 ++ nixpkgs.lib.filesystem.listFilesRecursive ./config;
         };
     };
