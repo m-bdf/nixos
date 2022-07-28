fdd49e8cd9475d465f5af8abaa115bf61dfce155
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-07-28 17:44:15 +0200

    sudo pidof

--- flake.nix
+++ flake.nix
@@ -21,9 +21,9 @@
             export NIX_USER_CONF_FILES=${nix-conf-file}
             echo building Nix config... >&2 && nix show-config >&2
 
-            pidof nix-daemon
+            sudo pidof nix-daemon
             sudo pkill nix-daemon
-            pidof nix-daemon
+            sudo pidof nix-daemon
 
             ${pkgs.nixos-rebuild}/bin/nixos-rebuild build \
                 --flake ${self}#mae --max-jobs auto \
