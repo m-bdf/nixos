e482ef070aec3ce1487a93ff6e2bf5c0ce8e6a5c
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-08-10 23:57:13 +0200

    pkgs.nixos

--- flake.nix
+++ flake.nix
@@ -5,15 +5,14 @@
         pkgs = nixpkgs.legacyPackages.${system};
 
     in {
-        nixosConfigurations.mae = nixpkgs.lib.nixosSystem {
-            inherit system;
-            modules = [ ./hardware-configuration.nix ]
-                ++ nixpkgs.lib.filesystem.listFilesRecursive ./config;
-        };
+        nixosConfigurations.mae = pkgs.nixos (
+            [ ./hardware-configuration.nix ]
+                ++ nixpkgs.lib.filesystem.listFilesRecursive ./config
+        );
 
         packages.${system}.default =
             pkgs.writeText "deploy.json" (builtins.toJSON {
-                agents.mae = self.nixosConfigurations.mae;
+                agents.mae = self.nixosConfigurations.mae.toplevel;
             });
     };
 }
