7a7deb94b6280bf96e54893cfd24e77306cf20b2
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-09-07 21:57:28 +0000

    Add user and reset system name

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
-    outputs = { self, nixpkgs }: {
-        nixosConfigurations.mae = nixpkgs.lib.nixosSystem {
+    outputs = { self, nixpkgs }:
+    let
+        system = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [ ./hardware-configuration.nix ]
                 ++ nixpkgs.lib.filesystem.listFilesRecursive ./config;
         };
-    };
+
+    in { nixosConfigurations.${system.config.system.name} = system; };
 }
