cff9e594f3a283073b1d9231922ea8e95b5a4057
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-09-15 23:32:08 +0000

    Enable networkd with iwd

---
+++ config/network.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+
+{
+    networking = {
+        useNetworkd = true;
+
+        wireless.iwd = {
+            enable = true;
+
+            settings = {
+                General = {
+                    EnableNetworkConfiguration = true;
+                    AddressRandomization = "network";
+                };
+                Network.EnableIPv6 = true;
+            };
+        };
+    };
+
+    systemd = {
+        targets.network-online.wantedBy = lib.mkForce []; #86273
+        network.wait-online.anyInterface = true;
+    };
+}
