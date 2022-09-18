2423e95285841efe21d1579727a86a8fb28c5e0e
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-09-18 16:24:30 +0000

    Don't recurse into config/

--- flake.nix
+++ flake.nix
@@ -3,10 +3,16 @@
 
     outputs = { self, nixpkgs, impermanence }:
     let
+        listDir = dir: map (entry: dir + "/${entry}")
+            (builtins.attrNames (builtins.readDir dir));
+
         system = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
-            modules = nixpkgs.lib.filesystem.listFilesRecursive ./config
-                ++ [ ./hardware-configuration.nix impermanence.nixosModule ];
+
+            modules = listDir ./config ++ [
+                impermanence.nixosModule
+                ./hardware-configuration.nix
+            ];
         };
 
     in { nixosConfigurations.${system.config.system.name} = system; };
