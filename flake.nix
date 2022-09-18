8633f3b8560ec5ac7e088e8e7368e36c709f67db
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-09-18 16:29:30 +0000

    Import local TOML

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
-    inputs.secrets.url = "github:divnix/blank";
-
-    outputs = { self, nixpkgs, secrets }:
+    outputs = { self, nixpkgs }:
     let
         system = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
-            specialArgs.secrets = secrets.outputs;
+            specialArgs.secrets = nixpkgs.lib.importTOML ./secrets;
             modules = [ ./hardware-configuration.nix ]
                 ++ nixpkgs.lib.filesystem.listFilesRecursive ./config;
         };
