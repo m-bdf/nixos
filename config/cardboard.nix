9dcbdad2494516afadcfa8ca672972b277f87a5f
Maëlys Bras de fer <mae.bdf@outlook.com>
2022-10-27 04:00:57 +0200

    cardboard

---
+++ config/cardboard.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+let
+  rc =
+  let
+    inherit (config.environment) defaultTerminal defaultBrowser;
+  in
+    pkgs.writeShellScript "cardboardrc" ''
+      target="wayland-instance@$WAYLAND_DISPLAY.target"
+      #trap "systemctl --user stop $target" TERM
+      systemctl --user start $target
+
+      cutter bind alt+Return exec "${defaultTerminal}"
+      cutter bind super+Return exec "${defaultBrowser}"
+
+      cutter bind alt+Left focus left
+      cutter bind alt+Right focus right
+      cutter bind super+Left move -10 0
+      cutter bind super+Right move 10 0
+
+      cutter bind alt+Backspace close
+      cutter bind super+Escape quit
+    '';
+in
+
+{
+  environment = {
+    systemPackages = [ pkgs.cardboard ];
+    etc."cardboard/cardboardrc".source = rc;
+  };
+}
