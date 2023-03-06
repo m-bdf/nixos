5beaee67146633a50db0dc40109c20f64499862b
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-03-06 23:44:55 +0100

    XDG basedirs creation and persistence

--- config/nix.nix
+++ config/nix.nix
@@ -1,10 +1,22 @@
-{ lib, nixpkgs, ... }:
+{ lib, pkgs, nixpkgs, ... }:
 
 {
   system.stateVersion = lib.trivial.release;
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      nix = final.nixVersions.unstable;
+    })
+  ];
+
   nix = {
     registry.nixpkgs.flake = nixpkgs;
     nixPath = [ "nixpkgs=${nixpkgs}" ];
+    settings.use-xdg-base-directories = true;
+  };
+
+  xdg.basedirs = {
+    cache.nix.persist = true; # tarballs
+    data.nix.persist = true; # REPL history
   };
 }
