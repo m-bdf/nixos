d4f15b0bee976729bdbbdb6f8305795930767c20
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-04-04 02:01:56 +0200

    Update settings for iwd 2.0

--- config/network.nix
+++ config/network.nix
@@ -6,20 +6,15 @@
 
     wireless.iwd = {
       enable = true;
-      settings = {
-        General = {
-          EnableNetworkConfiguration = true;
-          AddressRandomization = "network";
-        };
-        Network.EnableIPv6 = true;
+      settings.General = {
+        EnableNetworkConfiguration = true;
+        AddressRandomization = "network";
+        DisableANQP = false;
       };
     };
   };
 
-  systemd = {
-    targets.network-online.wantedBy = lib.mkForce []; #86273
-    network.wait-online.anyInterface = true;
-  };
+  systemd.targets.network-online.wantedBy = lib.mkForce []; #86273
 
   xdg.basedirs.state.iwd.persist = true; # networks
 }
