3f4bb34e4a3adf7fa5ccdb558efabf958809d84f
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-07-11 21:04:01 +0200

    Disable IPv6 and DNSSEC

--- config/network.nix
+++ config/network.nix
@@ -6,18 +6,18 @@
 
     wireless.iwd = {
       enable = true;
-      settings.General = {
-        EnableNetworkConfiguration = true;
-        AddressRandomization = "network";
-        DisableANQP = false;
+      settings = {
+        General = {
+          EnableNetworkConfiguration = true;
+          AddressRandomization = "network";
+          DisableANQP = false;
+        };
+        Network.EnableIPv6 = false;
       };
     };
   };
 
-  systemd = {
-    targets.network-online.wantedBy = lib.mkForce []; #86273
-    network.wait-online.anyInterface = true;
-  };
+  systemd.targets.network-online.wantedBy = lib.mkForce []; #86273
 
   xdg.dirs.state.iwd.persist = true; # networks
 }
