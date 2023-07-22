20de8632ed6444962e6c1299ffab57e614feb412
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-07-23 01:33:36 +0200

    Starship workarounds

--- config/shell.nix
+++ config/shell.nix
@@ -17,17 +17,21 @@
         preset = pkgs.runCommandLocal "starship-nerd-font-preset" {}
           "${lib.getExe pkgs.starship} preset nerd-font-symbols > $out";
       in
-        lib.importTOML preset;
+        lib.importTOML preset // { command_timeout = 2500; };
     };
   };
 
   imports = [ nix-index-database.nixosModules.nix-index ];
 
-  environment.systemPackages = [
-    (pkgs.comma.override {
-      nix-index-unwrapped = config.programs.nix-index.package;
-    })
-  ];
+  environment = {
+    systemPackages = [
+      (pkgs.comma.override {
+        nix-index-unwrapped = config.programs.nix-index.package;
+      })
+    ];
+
+    sessionVariables.STARSHIP_CACHE = "/var/cache/starship"; #starship/896
+  };
 
   fonts.fonts = [
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
@@ -36,5 +40,6 @@
   xdg.dirs = {
     config.fish.create = true; # variables
     data.fish.persist = true; # history
+    cache.starship.create = true; # logs
   };
 }
