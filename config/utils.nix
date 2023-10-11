4f72bb9b8f99ee870d161e64c4c590a99981e43a
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-10-11 17:22:17 +0200

    Core utils and apps

---
+++ config/utils.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  environment = {
+    defaultPackages = [];
+    systemPackages = with pkgs;
+    let
+      coreutils = uutils-coreutils.override { prefix = null; };
+    in
+      [ coreutils fd ripgrep helix xdg-utils kitty brave ];
+
+    etc."kitty/kitty.conf".text = ''
+      shell_integration enabled
+      enable_audio_bell no
+      confirm_os_window_close 0
+    '';
+
+    variables = {
+      EDITOR = "hx";
+      NIXOS_OZONE_WL = "1";
+    };
+  };
+
+  xdg.dirs = {
+    config."BraveSoftware/Brave-Browser".persist = true;
+    cache = {
+      helix.create = true;
+      kitty.create = true;
+      "BraveSoftware/Brave-Browser".create = true;
+    };
+  };
+}
