ec9beff2bc5f724942e548367abfc2253cdc1073
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-10-12 18:28:50 +0200

    Development tools

---
+++ config/code.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+  environment = {
+    systemPackages = with pkgs; [ github-desktop vscode nixd ];
+    sessionVariables.VSCODE_PORTABLE = "/usr/share/vscode"; #vscode/3884
+
+    variables.FILTER_BRANCH_SQUELCH_WARNING = "1";
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    config = [{
+      user = {
+        name = "Maëlys Bras de fer";
+        email = "mae.bdf@outlook.com";
+      };
+
+      alias.reset-commiter-date = "filter-branch --force " +
+        "--env-filter 'export GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE'";
+    }];
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  xdg.dirs = {
+    config."GitHub Desktop".persist = true;
+    data = {
+      vscode.persist = true;
+      keyrings.persist = true;
+    };
+  };
+}
