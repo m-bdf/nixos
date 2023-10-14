b21b6d45ffb87b2d6609053251bce01cdaf43b37
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-10-14 20:43:11 +0200

    Dracula theme, wallpaper and fonts
    theme

---
+++ config/theme.nix
@@ -0,0 +1,39 @@
+{ pkgs, dracula-fuzzel, dracula-kitty, ... }:
+
+{
+  environment = {
+    systemPackages = with pkgs; [ dracula-theme gnome.adwaita-icon-theme ];
+    sessionVariables.GTK_THEME = "Dracula";
+
+    etc = {
+      fuzzel.source = dracula-fuzzel;
+      "kitty/kitty.conf".text = "include ${dracula-kitty}/dracula.conf";
+      "helix/config.toml".text = "theme = \"github_dark\"";
+    };
+  };
+
+  fonts = {
+    enableDefaultPackages = false;
+    packages = with pkgs;
+    let
+      fira-code = nerdfonts.override { fonts = [ "FiraCode" ]; };
+    in
+      [ fira-code noto-fonts noto-fonts-cjk-sans noto-fonts-color-emoji ];
+
+    fontconfig.defaultFonts =
+    let
+      fonts = [ "FiraCode Nerd Font" "Noto Color Emoji" ];
+    in
+    {
+      sansSerif = [ "Noto Sans" ] ++ fonts;
+      serif = [ "Noto Serif" ] ++ fonts;
+      monospace = fonts;
+      emoji = fonts;
+    };
+  };
+
+  xdg.dirs.cache = {
+    mesa_shader_cache.create = true;
+    fontconfig.create = true;
+  };
+}
