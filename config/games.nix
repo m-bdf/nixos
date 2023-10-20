62d64993dd4b9825ecf8a1bf8f8a335bf581e5a8
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-10-20 02:16:53 +0200

    Lutris and PrismLauncher

---
+++ config/games.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages =
+  let
+    lutris-with-wayland = pkgs.lutris.override {
+      extraPkgs = pkgs: [ pkgs.wineWowPackages.waylandFull ];
+    };
+
+    lutris = pkgs.buildFHSEnv
+      (lutris-with-wayland.args // {
+        extraBwrapArgs = [
+          "--bind /usr/share /usr/share"
+          "--bind /etc/xdg /etc/xdg"
+          "--bind /var /var"
+        ] ++ map (exe:
+          "--bind ${exe} /var/cache/winetricks/corefonts/${exe.name}"
+        ) pkgs.corefonts.exes;
+      });
+  in
+    [ lutris pkgs.prismlauncher ];
+
+  hardware.opengl.driSupport32Bit = true;
+
+  xdg.dirs = {
+    data = {
+      lutris.persist = true;
+      icons.create = true;
+      PrismLauncher.persist = true;
+    };
+    config.lutris.persist = true;
+    cache = {
+      lutris.create = true;
+      winetricks.create = true;
+    };
+    home.Games.persist = true;
+  };
+}
