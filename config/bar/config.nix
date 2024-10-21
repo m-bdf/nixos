59aba549ce2358a0124202e0794daf5a11d1ee57
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-10-22 00:50:59 +0200

    Unify font family and size

--- config/bar/config.nix
+++ config/bar/config.nix
@@ -11,31 +11,31 @@
   };
 
   network = {
-    format = "󰤭 Disconnected";
-    format-ethernet = "󰈀 {ifname}";
-    format-wifi = "{icon} {essid}";
+    format = "󰤭  Disconnected";
+    format-ethernet = "󰈀  {ifname}";
+    format-wifi = "{icon}  {essid}";
     format-icons = [ "󰤟" "󰤢" "󰤥" "󰤨" ];
     tooltip-format = "{ipaddr}";
     on-click = lib.getExe pkgs.iwgtk;
   };
 
   bluetooth = {
-    format = "󰂲 Disconnected";
-    format-connected = "󰂱 {device_alias}";
+    format = "󰂲  Disconnected";
+    format-connected = "󰂱  {device_alias}";
     on-click = lib.getExe pkgs.overskride;
     on-click-right = lib.getExe pkgs.blueberry;
   };
 
   wireplumber = {
-    format-muted = "󰝟 {volume}%";
-    format = "{icon} {volume}%";
+    format-muted = "󰝟  {volume}%";
+    format = "{icon}  {volume}%";
     format-icons = [ "󰕿" "󰖀" "󰕾" ];
     on-click = lib.getExe pkgs.pwvucontrol;
     on-click-right = lib.getExe pkgs.pavucontrol;
   };
 
   battery = {
-    format = "{icon} {capacity}%";
+    format = "{icon}  {capacity}%";
     format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
     states.critical = 5;
   };
