ccfb3fae6762a986adb258ed243072414b18286f
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-11-07 23:58:35 +0100

    Enable nixpkgs experiments

---
+++ config/pkgs.nix
@@ -0,0 +1,24 @@
+{ lib, modulesPath, ... }:
+
+{
+  imports = [ /${modulesPath}/profiles/perlless.nix ];
+
+  system.forbiddenDependenciesRegexes = lib.mkForce [];
+
+  nixpkgs.config = {
+    warnUndeclaredOptions = true;
+
+    allowAliases = false;
+    allowUnfree = true;
+    checkMeta = true;
+
+    doCheckByDefault = true;
+    strictDepsByDefault = true;
+    structuredAttrsByDefault = true;
+    enableParallelBuildingByDefault = true;
+    configurePlatformsByDefault = true;
+    contentAddressedByDefault = true;
+
+    replaceStdenv = { pkgs }: pkgs.zigStdenv;
+  };
+}
