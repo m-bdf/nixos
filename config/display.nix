1022d3edf97abef439d8b9a208bb02e9f8e7024a
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-12-20 22:52:18 +0100

    Switch to BeaconDB and UTF-8 only

--- config/display.nix
+++ config/display.nix
@@ -2,6 +2,11 @@
 
 {
   i18n.defaultLocale = "en_IE.UTF-8";
+  nixpkgs.overlays = [
+    (final: prev: {
+      glibcLocales = final.glibcLocalesUtf8;
+    })
+  ];
 
   environment.variables.XKB_DEFAULT_LAYOUT = "eu";
   systemd.globalEnvironment.XKB_DEFAULT_LAYOUT = "eu";
@@ -19,7 +24,12 @@
       executable = "/bin/gammastep";
     };
 
-    localtimed.enable = true;
+    automatic-timezoned.enable = true;
+    geoclue2 = {
+      geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+      submissionUrl = "https://api.beacondb.net/v2/geosubmit";
+      submitData = true;
+    };
   };
   location.provider = "geoclue2";
 }
