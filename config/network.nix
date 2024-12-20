a19018e01afbf43f53363f2e15875dea5c2b5164
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-12-20 22:51:31 +0100

    network

--- config/network.nix
+++ config/network.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   networking = {
@@ -12,9 +12,43 @@
         DisableANQP = false;
       };
     };
+
+    nftables.enable = true;
+    # firewall.allowedTCPPorts = [ 5555 ];
   };
 
-  systemd.targets.network-online.wantedBy = lib.mkForce []; #86273
+  systemd = {
+    targets.network-online.wantedBy = lib.mkForce []; #86273
 
-  xdg.dirs.state.iwd.persist = true; # networks
+    user.services.wayvnc = {
+      inherit (pkgs.wayvnc.meta) description;
+      serviceConfig = {
+        ExecStart = lib.getExe pkgs.wayvnc;
+        Restart = "always";
+      };
+
+      partOf = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+    };
+  };
+
+  services = {
+    softether = {
+      # enable = true;
+      vpnserver.enable = true;
+    };
+
+    # avahi = {
+    #   enable = true;
+    #   nssmdns4 = true;
+    #   nssmdns6 = true;
+    # };
+    # printing.enable = true;
+  };
+
+  xdg.dirs.state = {
+    iwd.persist = true; # networks
+    softether.create = true;
+  };
 }
