4c2c9c7c389c65ae15588b77885f9adbae7c1bdc
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-01-07 17:05:18 +0100

    Use musl and zig by default
    musl

--- config/pkgs.nix
+++ config/pkgs.nix
@@ -5,11 +5,21 @@
 
   system.forbiddenDependenciesRegexes = lib.mkForce [];
 
-  nixpkgs.config = {
-    warnUndeclaredOptions = true;
+  nixpkgs = {
+    config = {
+      warnUndeclaredOptions = true;
 
-    allowAliases = false;
-    allowUnfree = true;
-    checkMeta = true;
+      allowAliases = false;
+      allowUnfree = true;
+      checkMeta = true;
+
+      replaceStdenv = { pkgs }: pkgs.stdenv;
+    };
+
+    overlays = [
+      (final: prev:
+        lib.optionalAttrs (prev.config ? replaceStdenv) prev.pkgsZig.pkgsMusl
+      )
+    ];
   };
 }
