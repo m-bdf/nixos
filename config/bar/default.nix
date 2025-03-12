2992642fb0aa831fa9aa1798dd3fad9cf3e059e9
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-03-12 03:48:05 +0100

    GTK4, icon and cursor themes

--- config/bar/default.nix
+++ config/bar/default.nix
@@ -25,11 +25,10 @@
     "xdg/waybar/config".text = builtins.toJSON (import ./config.nix args);
 
     "xdg/waybar/style.css".text = ''
-      window { border-radius: 0 }
       button { padding: 0 }
       label { padding: 5px 10px }
 
-      #workspaces > .active { background: @selected_bg_color }
+      #workspaces > .active { background: @theme_selected_bg_color }
       @keyframes blink { from { background: @error_color } }
       .critical:not(.charging) { animation: blink .5s infinite }
     '';
