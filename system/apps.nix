2b0c2ffd2ba65091b7e153571cff5c7940d8cbba
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-10-16 11:00:24 +0200

    apps

--- system/apps.nix
+++ system/apps.nix
@@ -2,31 +2,17 @@
 
 let
   wrapSpawn = name: cmd: pkgs.writeShellScriptBin name ''
-    niri msg action spawn -- ${pkgs.writeShellScript name cmd} "$@"
+    niri msg action spawn -- sh -c 'cd "$0" && ${cmd}' "$PWD" "$@"
   '';
 in
 
 {
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     niri = prev.niri.overrideAttrs {
-  #       prePatch = ''
-  #         sed -i 's/0 => ()/0 => \
-  #           if let Some(pipe) = pipe_wait_read { \
-  #             let raw = pipe.as_raw_fd() as u32; \
-  #             let _ = close_range(0, raw - 1, 0); \
-  #             let _ = close_range(raw + 1, !0, 0); \
-  #             let _ = read_all(pipe, \&mut [0]); \
-  #           }/' src/utils/spawning.rs
-  #       '';
-  #     };
-  #   })
-  # ];
-
   environment = {
     systemPackages = with pkgs;
     let
-      xdg-open = wrapSpawn "xdg-open" ''${pkgs.glib}/bin/gio open "$@"'';
+      xdg-open = wrapSpawn "xdg-open" ''
+        sleep 1 && ${pkgs.glib}/bin/gio open "$@"
+      '';
     in
       [ xdg-open ghostty nautilus brave ];
 
@@ -55,7 +41,7 @@ in
   xdg.terminal-exec = {
     enable = true;
     package = wrapSpawn "xdg-terminal-exec" ''
-      ${lib.getExe pkgs.xdg-terminal-exec} --dir="$PWD" "''${@-:$SHELL}"
+      ${lib.getExe pkgs.xdg-terminal-exec-mkhl} "''${@:-$SHELL}"
     '';
   };
 
