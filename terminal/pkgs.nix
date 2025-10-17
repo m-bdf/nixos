de2b8e0c06b17f556d98bf254d68aeb2efc45ea6
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-10-17 11:17:21 +0200

    pkgs 9

--- terminal/pkgs.nix
+++ terminal/pkgs.nix
@@ -1,68 +1,60 @@
 { options, lib, pkgs, ... }:
 
+with lib;
+
 let
   typeToPretty = o: t: {
     inherit (t) name description deprecationMessage;
 
-    nestedTypes = lib.optionalAttrs (
+    nestedTypes = optionalAttrs (
       t.name == "attrsOf" &&
       t.nestedTypes.elemType.name != o.type.name
     ) {
       elemType = typeToPretty o t.nestedTypes.elemType;
     };
 
-    getSubOptions = lib.optionalAttrs (
+    getSubOptions = optionalAttrs (
       t.name == "submodule" &&
-      lib.elem o.visible or true [ true "transparent" ]
+      elem o.visible or true [ true "transparent" ]
     ) {
       val = optsToPretty (t.getSubOptions o.loc);
-      __pretty = subOpts: "_:\n${subOpts} # ${o}\n";
+      __pretty = opts: "_:\n${opts} # ${o}\n";
     };
   };
 
   optsToPretty = opts:
   let
-    docs = lib.listToAttrs (
-      map (doc: doc // { value = doc; })
-        (lib.optionAttrSetToDocList opts)
-    );
+    docs = groupBy (o: o.name) (optionAttrSetToDocList opts);
 
-    prettyOpts =
-      lib.mapAttrsRecursiveCond (v: !lib.isOption v)
-        (_: o: docs.${toString o} // {
-          inherit (o) _type declarationPositions;
-          type = typeToPretty o o.type;
-          default = null;
-        })
-        (lib.removeAttrs opts [ "_module" ]);
+    prettyOpts = mapAttrsRecursiveCond (v: !isOption v)
+      (_: o: head docs.${toString o} // {
+        inherit (o) _type declarationPositions;
+        type = typeToPretty o o.type;
+        default = null;
+      })
+      (removeAttrs opts [ "_module" ]);
   in
-    lib.generators.toPretty {
-      allowPrettyValues = true;
-    } prettyOpts;
+    generators.toPretty { allowPrettyValues = true; } prettyOpts;
 
   modules = pkgs.writeTextDir "module-list.nix" ''
     let
       mkModule = n: o:
-        { lib, ... }:
+        { lib, ... }: with lib;
 
         {
           options.''${n} =
-            if lib.isOption o then o
-            else lib.mkOption {
-              type = lib.mkOptionType {
+            o // mkOption {
+              type = mkOptionType {
                 name = "submodule";
                 getSubOptions = _: o;
               };
             };
         };
     in
-      builtins.attrValues (
-        builtins.mapAttrs mkModule
-          (import ${
-            pkgs.writeText "merged-options.nix" (optsToPretty
-              (lib.recursiveUpdate (pkgs.nixos {}).options options)
-            )
-          })
+      with builtins; attrValues (mapAttrs mkModule
+        (import ${pkgs.writeText "merged-options.nix"
+          (optsToPretty (recursiveUpdate (pkgs.nixos {}).options options))
+        })
       )
   '';
 in
