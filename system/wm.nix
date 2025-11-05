67a0be4125e79dd21d5bd5776a9cd2a5585a4fca
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-11-05 09:00:49 +0100

    wallpaper

--- system/wm.nix
+++ system/wm.nix
@@ -11,8 +11,8 @@
       enable = true;
       startup = "${lib.getExe pkgs.swaybg} --image ${pkgs.fetchurl rec {
         name = "melynx-sylveon-garden.png";
-        url = "https://weasyl.com/~melynx/submissions/1182575/${name}";
-        hash = "sha256-pb5MoWtXoiMs0R/ViXrUU64u2lVIJujv0CUzs/Wc/TU=";
+        url = "https://weasyl.com/~melynx/submissions/1182575/${sha256}/${name}";
+        sha256 = "a5be4ca16b57a2232cd11fd5897ad453ae2eda554826e8efd02533b3f59cfd35";
       }} --mode fill";
     };
 
