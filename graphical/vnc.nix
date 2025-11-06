3971033f998055db5838c351e274b82c7a2706a6
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-11-07 00:26:27 +0100

    Move VNC to HM

---
+++ graphical/vnc.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+let
+  wayvnc = pkgs.wayvnc.overrideAttrs {
+    patches = [
+      (pkgs.fetchpatch {
+        url = "https://github.com/any1/wayvnc/pull/396.patch";
+        hash = "sha256-IEfHVhWj075DQ+HRnGL8zhsVaj813UWEOiYnOUYS/50=";
+      })
+    ];
+  };
+in
+
+{
+  services.wayvnc = {
+    enable = true;
+    package = wayvnc;
+    autoStart = true;
+
+    settings = {
+      address = "0.0.0.0";
+      port = 5900;
+
+      enable_auth = true;
+      enable_pam = true;
+      relax_encryption = true;
+    };
+  };
+}
