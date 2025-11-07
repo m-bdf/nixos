a36d207e3368509385e6511ae86c9f60b35d87f1
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-11-07 01:00:08 +0100

    Move theme back to system

--- system/theme.nix
+++ system/theme.nix
@@ -1,4 +1,44 @@
+{ pkgs, ... }:
+
 {
+  home = {
+    gtk = {
+      enable = true;
+      gtk2.enable = false;
+
+      theme = {
+        name = "Colloid-Dark-Compact-Dracula";
+        package = pkgs.colloid-gtk-theme.override {
+          colorVariants = [ "dark" ];
+          sizeVariants = [ "compact" ];
+          tweaks = [ "dracula" ];
+        };
+      };
+
+      iconTheme = {
+        name = "Colloid-Dracula-Dark";
+        package = pkgs.colloid-icon-theme.override {
+          schemeVariants = [ "dracula" ];
+        };
+      };
+
+      cursorTheme = {
+        name = "phinger-cursors-dark";
+        package = pkgs.phinger-cursors;
+      };
+    };
+
+    dconf.settings = {
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/desktop/wm/preferences".button-layout = "";
+    };
+
+    qt = {
+      enable = true;
+      platformTheme.name = "gtk3";
+    };
+  };
+
   environment.etc = {
     "xdg/niri/config.kdl".text = ''
       cursor { xcursor-theme "phinger-cursors-dark"; }
