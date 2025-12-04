08f8b3eb143d7dfa5252abd99441e55f63f277e8
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-12-04 11:17:25 +0100

    first asserts rewrite

--- asserts.nix
+++ asserts.nix
@@ -1,85 +1,88 @@
-{ _prefix, options, config, lib, modules, extendModules, ... }@ self:
+{ _prefix, options, config, lib, modules, moduleType, extendModules, ... }@ self:
 
 with lib;
 
 let
   dropPrefix = drop (length _prefix);
 
-  collectModules = lib.modules.collectModules "";
-  collectedModules = (collectModules modules self).modules;
-
-  mkRedundantAssert = loc: optValue: file: defValue:
-  let
-    removeAttrByPath = path: set:
-      mkMerge (forEach (pushDownProperties set) (set:
-        if length path == 1 then removeAttrs set path else set // {
-          ${head path} = removeAttrByPath (tail path) set.${head path} or {};
-        }
-      ));
-
-    systemWithoutDef = extendModules {
-      modules = forEach collectedModules
-        (m: if m._file != file then m else {
-          disabledModules = [m];
-          inherit (m) _file options;
-          config = removeAttrByPath (dropPrefix loc) m.config;
-        });
+  evalFreeform = module:
+    evalModules {
+      specialArgs = self;
+      modules = [ module rec {
+        freeformType = with types;
+          either (attrsOf freeformType) unspecified;
+      }];
     };
 
-    valueWithoutDef = getAttrFromPath (dropPrefix loc) systemWithoutDef.config;
+  collectModules =
+    zipListsWith (meta: module:
+      optional (elem module modules) {
+        inherit (meta) key file;
+        inherit (evalFreeform module) options config;
+      } ++
+        collectModules meta.imports module.imports or []
+    );
 
-    prettyOpt = "option `${showOption loc}' defined in `${file}'";
-    prettyVal = generators.toPretty { multiline = false; } defValue;
+  collectedModules = flatten
+    (collectModules (extendModules {}).graph moduleType.getSubModules);
+
+  mkRedundantAssert = opt: def:
+  let
+    removeAttrByPath = path: set:
+      if length path == 1 then removeAttrs set path else set // {
+        ${head path} = removeAttrByPath (tail path) set.${head path} or {};
+      };
+
+    noDefSystem = extendModules {
+      modules = [{
+        disabledModules = [def];
+        options = removeAttrs def.options [ "_module" ];
+        config = removeAttrByPath (dropPrefix opt.loc) def.config;
+      }];
+    };
+
+    noDefVal = getAttrFromPath (dropPrefix opt.loc) noDefSystem.config;
+    onlyDefVal = getAttrFromPath (dropPrefix opt.loc) def.config;
+
+    prettyOpt = "option `${showOption opt.loc}' defined in `${def.file}'";
+    prettyVal = generators.toPretty { multiline = false; } onlyDefVal;
   in
   {
     assertion = builtins.traceVerbose "Checking the ${prettyOpt}…"
-      (!(builtins.tryEval (valueWithoutDef == optValue)).value);
+      (!(builtins.tryEval (noDefVal == opt.value)).value);
 
     message = "The ${prettyOpt} is set to the redundant value `${prettyVal}'.";
   };
 
-  mkRedundantAsserts =
+  mkRedundantAsserts = opt:
   let
-    userFiles = catAttrs "_file" (take (length modules) collectedModules);
-    filterUserModules = filter (m: elem m.file or m._file userFiles);
-
-    freeform = evalModules {
-      modules = [ rec {
-        freeformType = with types;
-          either (attrsOf freeformType) unspecified;
-        config = mkMerge (catAttrs "config" collectedModules);
-      }];
-    };
-
-    subModule = { moduleType, ... }@ self: {
-      _module.args.modules = filterUserModules
-        (collectModules moduleType.getSubModules self).modules;
+    subModule = file: { moduleType, ... }: {
+      _module.args.modules = filter (m: m._file or m == file)
+        (subtractLists opt.type.getSubModules moduleType.getSubModules);
     };
 
     collectAsserts = v: v.assertions or
-      (concatMap collectAsserts (if isAttrs v then attrValues v else v));
+      (forEach (if isAttrs v then attrValues v else v) collectAsserts);
   in
-    concatMap (opt: optionals (
+    forEach collectedModules (m: optionals (
       !elem (last opt.loc) [ "assertions" "warnings" "stateVersion" ] && #176295
       !hasPrefix "Alias" opt.description or "" && #355488
-      hasAttrByPath (dropPrefix opt.loc) freeform.config
+      hasAttrByPath (dropPrefix opt.loc) m.config &&
+      elem m.file opt.files
     ) (
-      forEach (filterUserModules opt.definitionsWithLocations)
-        (def: mkRedundantAssert opt.loc opt.value def.file def.value)
-    ++
-      optionals (opt.type.getSubModules != null) (
+      if opt.type.getSubModules != null then
         collectAsserts ((opt.type.substSubModules (
-          opt.type.getSubModules ++ [ subModule __curPos.file ]
+          opt.type.getSubModules ++ [ (subModule m.file) __curPos.file ]
         )).merge opt.loc opt.definitionsWithLocations)
-      )
+      else
+        [ (mkRedundantAssert opt m) ]
     ));
 in
 
 {
   options.assertions = mkOption {};
   config.assertions =
-    forEach config.warnings or [] (message:
-      { assertion = false; inherit message; }
-    ) ++
-      mkRedundantAsserts (collect isOption options);
+    forEach config.warnings or []
+      (message: { assertion = false; inherit message; }) ++
+    flatten (forEach (collect isOption options) mkRedundantAsserts);
 }
