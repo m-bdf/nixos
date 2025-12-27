261eb3da8c3547993ad99df1d6bfcb0e046729b1
Maëlys Bras de fer <mae.bdf@outlook.com>
2025-12-27 16:43:43 +0100

    Fix isatty

--- home/utils/isatty.c
+++ home/utils/isatty.c
@@ -1,60 +1,63 @@
-#include <dlfcn.h>
 #include <stdlib.h>
-#include <string.h>
 #include <limits.h>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <glob.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <glob.h>
 
-int isatty(int fd) {
+static int ispager(int len;
+  char procpath[len], int len)
+{
+  const char *pager = getenv("PAGER");
+  if (!pager || !pager[0]) return 0;
+
+  static char buf[PATH_MAX], *cmd = buf;
+  int fd;
+
+  strcpy(procpath + len, "/cmdline");
+  if ((fd = open(procpath, O_RDONLY)) < 0 ||
+    read(fd, buf, sizeof(buf)) < 0 ||
+    close(fd) < 0) return 0;
+
+  if (buf[0] == '/')
+    cmd = strrchr(buf, '/') + 1;
+  if (!strcmp(cmd, pager)) return 1;
+
+  strcpy(procpath + len, "/comm");
+  if ((fd = open(procpath, O_RDONLY)) < 0 ||
+    (len = read(fd, buf, sizeof(buf))) < 0 ||
+    close(fd) < 0) return 0;
+
+  return buf[len - 1] == '\n' &&
+    !strncmp(buf, pager, len - 1);
+}
+
+int isatty(int fd)
+{
   if (__isatty(fd)) return 1;
   if (fd != 1) return 0;
 
-  const char *pager = getenv("PAGER");
-  if (!pager || !pager[0]) return 0;
-  char comm[strlen(pager) + 1];
-
-  static char cmdline[PATH_MAX],
-    path[PATH_MAX] = "/dev/stdout";
-  const char *cmd = cmdline;
-
-  static struct stat statself, statbuf;
-  if (stat(path, &statself) < 0) return 0;
+  static struct stat statbuf;
+  if (stat("/dev/stdout", &statbuf) < 0) return 0;
+  ino_t ino = statbuf.st_ino;
 
   static glob_t globbuf;
   const int flags = GLOB_NOSORT | GLOB_ONLYDIR;
   glob("/proc/[0-9]*", flags, NULL, &globbuf);
 
-  for (int i = globbuf.gl_pathc; i--; ) {
-    strcpy(path, globbuf.gl_pathv[i]);
-    int len = strlen(path);
+  for (int i = globbuf.gl_pathc; i--; )
+  {
+    static char procpath[PATH_MAX];
+    strcpy(procpath, globbuf.gl_pathv[i]);
+    int len = strlen(procpath);
 
-    strcpy(path + len, "/fd/0");
-    if (stat(path, &statbuf) < 0 ||
-      statbuf.st_ino != statself.st_ino) continue;
+    strcpy(procpath + len, "/fd/0");
+    if (stat(procpath, &statbuf) < 0 ||
+      statbuf.st_ino != ino) continue;
 
     globfree(&globbuf);
-
-    strcpy(path + len, "/cmdline");
-    if ((fd = open(path, O_RDONLY)) < 0) return 0;
-
-    if (read(fd, cmdline, sizeof(cmdline)) < 0 ||
-      close(fd) < 0) return 0;
-
-    if (cmdline[0] == '/')
-      cmd = strrchr(cmdline, '/') + 1;
-    if (!strcmp(cmd, pager)) return 1;
-
-    strcpy(path + len, "/comm");
-    if ((fd = open(path, O_RDONLY)) < 0) return 0;
-
-    if ((len = read(fd, comm, sizeof(comm))) < 0 ||
-      close(fd) < 0) return 0;
-
-    return comm[len - 1] == '\n' &&
-      !strncmp(comm, pager, len - 1);
+    return ispager(procpath, len);
   }
 
   globfree(&globbuf);
