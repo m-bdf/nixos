ba2edf3cf392d6274b135afa3bab2c6e9316f494
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-01-06 16:44:50 +0100

    Add crane as flake.nix input

--- flake.nix
+++ flake.nix
@@ -33,6 +33,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    crane.url = "github:ipetkov/crane";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
