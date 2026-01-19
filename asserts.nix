5f0e572863f8346415b09ada5a93a0a7be0ee60d
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-01-19 02:06:08 +0100

    asserts ignore virtualisation

--- asserts.nix
+++ asserts.nix
@@ -93,5 +93,5 @@ in
     forEach config.warnings or []
       (message: { assertion = false; inherit message; }) ++
     concatMap mkRedundantAsserts
-      (collect isOption (removeAttrs options [ "assertions" "warnings" "meta" ])); # meta tmp
+      (collect isOption (removeAttrs options [ "assertions" "warnings" "meta" "virtualisation" ])); # meta & virtualisation tmp
 }
