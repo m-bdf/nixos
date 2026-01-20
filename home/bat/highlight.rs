5e9af611862674901c6885b1fa0b7af02e22c679
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-01-20 18:32:15 +0100

    readability

--- home/bat/highlight.rs
+++ home/bat/highlight.rs
@@ -5,7 +5,8 @@ impl HighlightingAssets {
         &self,
         reader: &mut InputReader,
     ) -> Result<Option<SyntaxReferenceInSet<'_>>> {
-        if reader.first_line.is_empty() || reader.first_line.contains(&b'\x1b') {
+        if reader.first_line.is_empty()
+        || reader.first_line.iter().any(u8::is_ascii_control) {
             return Ok(None);
         }
 
