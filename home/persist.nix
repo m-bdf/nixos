1b3df725f8a2a0d916833355f32158d735e466d3
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-01-29 06:57:24 +0100

    unsethome dag ??

--- home/persist.nix
+++ home/persist.nix
@@ -36,11 +36,12 @@ in
   config = {
     xdg.enable = true;
 
-    lib.bash = mkForce {
-      initHomeManagerLib = ''
-        source ${inputs.home-manager}/lib/bash/*
-        test $(basename $0) = activate || HOME=/
-      '';
+    home.activation = {
+      unsetHome = hm.dag.entryBefore
+        [ "linkGeneration" ] "HOME=/";
+      resetHome = hm.dag.entryBetween
+        [ "batCache" ] [ "linkGeneration" ]
+        "HOME=${config.home.homeDirectory}";
     };
 
     home.file = {
