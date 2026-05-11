5c226eff11b73a3f4e78c65289dc81bf9316c693
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-05-11 09:05:55 +0200

    rm Sonic Pi

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,34 +1,7 @@
-{ pkgs, ... }:
-
 {
-  environment = {
-    systemPackages = with pkgs; [ sonic-pi ];
-    variables.SONIC_PI_HOME = "$XDG_DATA_HOME/SonicPi";
-  };
-
   security.rtkit.enable = true;
   hardware.bluetooth.enable = true;
   home.services.playerctld.enable = true;
-
-  services.pipewire.extraConfig.pipewire = {
-    default-output."context.modules" = [{
-      name = "libpipewire-module-loopback";
-      args = {
-        "node.description" = "Default Output";
-        "audio.position" = [ "FL" "FR" ];
-        "capture.props" = {
-          "node.name" = "alsa_output.loopback";
-          "media.name" = "Default Output";
-          "media.class" = "Audio/Sink";
-        };
-        "playback.props" = {
-          "node.name" = "alsa_input.loopback";
-          "media.name" = "Default Output";
-          "node.passive" = true;
-        };
-      };
-    }];
-  };
 
   programs.niri.keybinds = {
     XF86AudioMute = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
@@ -41,7 +14,6 @@
   };
 
   home.xdg = {
-    dataFile.SonicPi.persist = true;
     stateFile = {
       wireplumber.persist = true;
       bluetooth.persist = true;
