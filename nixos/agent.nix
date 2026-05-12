d4fe6e24afaf6ee90409dcb1613f77860a11c5ab
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-05-13 01:54:20 +0200

    Add Cursor agents config

---
+++ nixos/agent.nix
@@ -0,0 +1,40 @@
+{ lib, pkgs, ... }:
+
+let
+  pickLatest = pkgs.writeShellScript "pick-latest" ''
+    while read pkg; do
+      nix derivation show nixpkgs#"$pkg" |
+      ${lib.getExe pkgs.jq} --arg pkg "$pkg" -r '
+        .derivations[] | .structuredAttrs // .env |
+        select(.name == .pname + "-" + .version) |
+        [$pkg, .version] | @tsv
+      '
+    done | sort -k2Vr | head -1 | cut -f1
+  '';
+in
+
+{
+  home.home.file = lib.concatMapAttrs (n: v: {
+    ".cursor/${n}.json".text = lib.toJSON v;
+  }) {
+    mcp.mcpServers = {
+      nix = {
+        command = lib.getExe pkgs.mcp-language-server;
+        args = [ "--workspace" "\${workspaceFolder}" "--lsp" "nixd" ];
+      };
+      nixos.command = lib.getExe pkgs.mcp-nixos;
+    };
+    permissions.mcpAllowlist = [ "*:*" ];
+
+    hooks = {
+      version = 1;
+      hooks.beforeShellExecution = [{
+        command = ''
+          command_not_found_handle() {
+            comma --picker ${pickLatest} "$@"
+          }
+        '';
+      }];
+    };
+  };
+}
