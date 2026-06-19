6bced2504ddcbb959205f5c0300601ff16585537
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-06-19 05:22:15 +0200

    Configure Zed agent

--- nixos/agent.nix
+++ nixos/agent.nix
@@ -1,10 +1,33 @@
 { lib, pkgs, ... }:
 
+let
+  pickLatest = pkgs.writeShellScript "pick-latest" ''
+    while read pkg; do
+      nix derivation show nixpkgs#"$pkg" |
+      ${lib.getExe pkgs.jaq} --arg pkg "$pkg" \
+        '.derivations[] | .structuredAttrs // .env
+        | select(.name == .pname + "-" + .version)
+        | [$pkg, .version]' --to tsv &
+    done 2>/dev/null |
+    sort -k2Vr | head -1 | cut -f1
+  '';
+in
+
 {
+  environment.binsh =
+    pkgs.writeShellScript "sh" ''
+      command_not_found_handle() {
+        comma --picker ${pickLatest} "$@";
+      }
+      export -f command_not_found_handle
+      exec "$BASH" "$@"
+    '';
+
   home.programs = {
     zed-editor = {
       enableMcpIntegration = true;
       extensions = [ "mcp-server-sequential-thinking" ];
+      userSettings.agent.tool_permissions.default = "allow";
     };
 
     mcp = {
