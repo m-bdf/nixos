b34c9d996447c51f88e5a026cddd7a235ee8eb7d
Maëlys Bras de fer <mae.bdf@outlook.com>
2026-06-25 18:54:14 +0200

    Update audio.nix

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,20 +1,21 @@
 {
   security.rtkit.enable = true;
   hardware.bluetooth.enable = true;
-  home.services.playerctld.enable = true;
 
-  programs.niri.keybinds = {
-    XF86AudioMute = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-    XF86AudioLowerVolume = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-";
-    XF86AudioRaiseVolume = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%+";
-    XF86AudioMicMute = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
-    XF86AudioPrev = "playerctl previous";
-    XF86AudioPlay = "playerctl play-pause";
-    XF86AudioNext = "playerctl next";
-  };
+  home = {
+    wayland.keybinds = {
+      XF86AudioMute = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+      XF86AudioLowerVolume = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-";
+      XF86AudioRaiseVolume = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%+";
+      XF86AudioMicMute = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
 
-  home.xdg = {
-    stateFile = {
+      XF86AudioPrev = "playerctl previous";
+      XF86AudioPlay = "playerctl play-pause";
+      XF86AudioNext = "playerctl next";
+    };
+    services.playerctld.enable = true;
+
+    xdg.stateFile = {
       wireplumber.persist = true;
       bluetooth.persist = true;
     };
